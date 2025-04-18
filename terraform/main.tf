f1832e320df001c66f42b37a9a69caef962ede1b
Chris <59842634+ChrisAtHashiCorp@users.noreply.github.com>
2025-04-18 00:52:13 -0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -32,4 +32,23 @@ resource "openstack_compute_instance_v2" "instance_rhl_wxk" {
     name = "${data.openstack_networking_network_v2.network.name}"
   }
   availability_zone = var.availability_zone
+
+  connection {
+    type     = "ssh"
+    user     = "root"
+    password = var.root_password
+    host     = self.public_ip
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "subscription-manager register --username ${var.rhsat_user} --password ${var.rhsat_pass}",
+    ]
+  }
+
+  provisioner "local-exec" {
+    inline = [
+      "ansible-playbook -u root -i ${self.ipv4_address} --private-key ${var.pvt_key} -e pub_key=${var.pub_key} DO-ALL-THE-THINGS.yml ADD-ME-TO-AAP.yml"
+    ]
+  }
 }
